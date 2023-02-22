9fae2198128f7d5a69ec46e40eb68946b504c088
Cuong Phan <cuong.phan@rovio.com>
2023-02-22 13:47:00 +0200

    init

---
+++ src/main/java/com/example/oneiofizzbuzz/OnEiOFizzBuzzApplication.java
@@ -0,0 +1,13 @@
+package com.example.oneiofizzbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnEiOFizzBuzzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnEiOFizzBuzzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oneiofizzbuzz/OnEiOFizzBuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oneiofizzbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnEiOFizzBuzzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
